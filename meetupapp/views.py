ee5e647b9689160f1f31d9ae148aa9506a9e6542
Austin Flatt <austindflatt@gmail.com>
2021-07-24 04:00:25 -0400

    cleaned up blank lines

--- meetupapp/views.py
+++ meetupapp/views.py
@@ -14,9 +14,7 @@ def registerPage(request):
     if request.user.is_authenticated:
         return redirect('home')
     else:
-
         form = CreateUserForm()
-
         if request.method == 'POST':
             form = CreateUserForm(request.POST)
             if form.is_valid():
@@ -36,9 +34,7 @@ def loginPage(request):
         if request.method == 'POST':
             username = request.POST.get('username')
             password = request.POST.get('password')
-
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
                 login(request, user)
                 return redirect('home')
