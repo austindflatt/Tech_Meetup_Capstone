fb0816b5fc465b85df8e4937083fc0378910786c
Austin Flatt <austindflatt@gmail.com>
2021-07-26 15:41:51 -0400

    fixed indentation problem in models and added models to admin.py

--- meetupapp/admin.py
+++ meetupapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import Meetup
+from . models import Meetup, Job
 
 # Register your models here.
 
 admin.site.register(Meetup)
+admin.site.register(Job)
--- meetupapp/models.py
+++ meetupapp/models.py
@@ -19,24 +19,24 @@ class Meetup(models.Model):
     def __str__(self):
         return self.title
 
-    class Job(models.Model):
-        title = models.CharField(max_length=200)
-        company = models.CharField(max_length=100)
-        description = models.TextField()
-        location = models.CharField(max_length=200)
-        type = models.CharField(max_length=200)
-        link = models.CharField(max_length=200)
-        created = models.DateTimeField(auto_now_add=True)
-        slug = models.SlugField(null=True, blank=True)
-        id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 
-    class Member(models.Model):
-        user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-        name = models.CharField(max_length=200, null=True)
-        profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
+class Member(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
