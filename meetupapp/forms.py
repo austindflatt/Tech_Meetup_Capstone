3b892386f1f40f0307e84959350605510fea7c5d
Austin Flatt <austindflatt@gmail.com>
2021-07-30 01:19:15 -0400

    fixed event form code so a user can select the date and time easily instead of manually inputting

--- meetupapp/forms.py
+++ meetupapp/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateTimeInput
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
@@ -16,10 +16,33 @@ class MeetupForm(ModelForm):
         model = Meetup
         fields = ['title', 'thumbnail', 'topic', 'description', 'city', 'address', 'meetup_date', 'end_date', 'creator']
 
+        widgets = {
+            'meetup_date': DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end_date': DateTimeInput(attrs={'type': 'time'}),
+        }
+
     def __init__(self, *args, **kwargs):
         super(MeetupForm, self).__init__(*args, **kwargs)
         self.fields['title'].widget.attrs.update(
-            {'class': ''}
+            {'placeholder': 'Choose a title for your event'}
+        )
+        self.fields['topic'].widget.attrs.update(
+            {'placeholder': 'What topic will be discussed?'}
+        )
+        self.fields['description'].widget.attrs.update(
+            {'placeholder': 'Please give a detailed description of what this event is about'}
+        )
+        self.fields['city'].widget.attrs.update(
+            {'placeholder': 'What city will this be in?'}
+        )
+        self.fields['address'].widget.attrs.update(
+            {'placeholder': 'What is the address?'}
+        )
+        self.fields['meetup_date'].widget.attrs.update(
+            {'placeholder': 'datetime-local'}
+        )
+        self.fields['end_date'].widget.attrs.update(
+            {'placeholder': 'datetime-local'}
         )
         self.fields['meetup_date'].widget.attrs.update(
             {'class': '',
@@ -35,5 +58,20 @@ class JobForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(JobForm, self).__init__(*args, **kwargs)
         self.fields['title'].widget.attrs.update(
-            {'class': ''}
+            {'placeholder': 'What is the name of the role you are hiring for?'}
+        )
+        self.fields['company'].widget.attrs.update(
+            {'placeholder': 'Company name'}
+        )
+        self.fields['description'].widget.attrs.update(
+            {'placeholder': 'Please give a detailed description about this job'}
+        )
+        self.fields['location'].widget.attrs.update(
+            {'placeholder': 'Type an address'}
+        )
+        self.fields['type'].widget.attrs.update(
+            {'placeholder': 'Full time or Part time?'}
+        )
+        self.fields['link'].widget.attrs.update(
+            {'placeholder': 'Company website'}
         )
